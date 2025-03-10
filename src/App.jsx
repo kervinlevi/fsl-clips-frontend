8a4e8b491a5d1c596c4c705c5c19f2aca32b1695
Kervin Levi Decena <kervinlevi@gmail.com>
2025-03-10 19:41:45 +0800

    Initial React.js project with Vite

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './features/home/Home'
+import Login from './features/login/Login'
+import './App.css'
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
