9334f7bacce00ee63e46a7a744cd6c136f9f91ec
Kervin Levi Decena <kervinlevi@gmail.com>
2025-04-18 19:06:47 +0800

    Add swipe functionality to home and show video description

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Home from './features/home/Home'
-import Login from './features/login/Login'
-import Registration from './features/register/Registration'
-import Dashboard from './features/dashboard/Dashboard'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./features/login/Login";
+import Registration from "./features/register/Registration";
+import Dashboard from "./features/dashboard/Dashboard";
+import WatchClips from "./features/home/WatchClips";
+import "./App.css";
 
 function App() {
   return (
@@ -13,11 +13,11 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Registration />} />
-        <Route path="/watch" element={<Home />} />
-        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path="/watch" element={<WatchClips />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/features/home/WatchClips.jsx
@@ -0,0 +1,204 @@
+import React, { useRef, useEffect, useState } from "react";
+import { useSwipeable } from "react-swipeable";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import _ from "lodash";
+
+const WatchClips = () => {
+  const navigate = useNavigate();
+  const videoRefs = useRef([]);
+  const [currentIndex, setCurrentIndex] = useState(-1);
+  const [clips, setClips] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const clipsLengthRef = useRef(0);
+
+  let lastClips = [];
+
+  const fetchRandomClips = async () => {
+    const exclude = encodeURIComponent(JSON.stringify(lastClips));
+    const url = `http://localhost:1337/randomClips?exclude=${exclude}`;
+
+    try {
+      const response = await axios.get(url);
+
+      // Remember most recent clips so they can be excluded next fetch
+      // This will only exclude the most recently fetched clips not all previously fetch clips
+      const newClips = response.data.clips;
+      lastClips = _.map(newClips, (clip) => clip.clip_id);
+
+      // Append newly fetched clips
+      setClips((currentClips) => [...currentClips, ...newClips]);
+      clipsLengthRef.current = clips.length;
+      if (currentIndex == -1) setCurrentIndex(0);
+      setLoading(false);
+      setError(null);
+    } catch (err) {
+      setError("Error fetching clips");
+      console.error("Error fetching clips: ", err);
+      setLoading(false);
+    }
+  };
+
+  // Fetch clips from backend
+  let firstFetch = true;
+  useEffect(() => {
+    if (!firstFetch) return;
+
+    firstFetch = false;
+    setLoading(true);
+    fetchRandomClips();
+  }, []);
+
+  // Handle video play/pause
+  useEffect(() => {
+    videoRefs.current.forEach((vid, idx) => {
+      if (vid) {
+        if (idx === currentIndex) {
+          vid.play().catch(() => {});
+        } else {
+          vid.pause();
+          vid.currentTime = 0;
+        }
+      }
+    });
+  }, [currentIndex]);
+
+  const showPreviousClip = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex(currentIndex - 1);
+    }
+  };
+
+  const showNextClip = () => {
+    if (currentIndex + 1 < clips.length) {
+      setCurrentIndex(currentIndex + 1);
+
+      // Fetch random clips when index is near the end
+      if (currentIndex > clips.length - 3) {
+        fetchRandomClips();
+      }
+    }
+  };
+
+  // On mount, disable scroll
+  // On unmount, reset this behavior
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, []);
+
+  const handleMenuClick = () => {
+    alert("You have been logged out");
+    navigate("/login", { replace: true });
+  };
+
+  const handleSwipeTap = () => {
+    if (currentIndex >= 0 && currentIndex < clips.length) {
+      const video = videoRefs.current[currentIndex];
+      if (!video) return;
+
+      if (video.paused) {
+        video.play().catch(() => {});
+      } else {
+        video.pause();
+      }
+    }
+  };
+
+  const handleSwipe = useSwipeable({
+    onSwipedUp: () => showNextClip(),
+    onSwipedDown: () => showPreviousClip(),
+    onTap: () => handleSwipeTap(),
+    preventScrollOnSwipe: true,
+    preventDefaultTouchmoveEvent: false,
+    trackTouch: true,
+    trackMouse: true,
+  });
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  return (
+    <div
+      {...handleSwipe}
+      className="min-h-screen bg-sky-blue relative overflow-hidden touch-none"
+    >
+      <div
+        style={{
+          height: `${clips.length * 100}vh`,
+          transform: `translateY(-${currentIndex * 100}vh)`,
+        }}
+        className="h-screen flex flex-col items-center transition-transform duration-500 ease-out"
+      >
+        {clips.map((clip, index) => (
+          <div key={index} className="h-screen aspect-9/16 relative">
+            <video
+              ref={(el) => (videoRefs.current[index] = el)}
+              src={`http://localhost:1337/${clip.video_url}`}
+              poster={`http://localhost:1337/${clip.thumbnail_url}`}
+              className="object-cover rounded-lg bg-space-cadet cursor-all-scroll"
+              muted
+              playsInline
+              loop
+              controls={false}
+              type="video/mp4"
+            />
+
+            <div className="absolute bottom-0 left-0 w-full p-4 z-10 bg-gradient-to-t from-black/70 to-transparent rounded-b-lg">
+              <h3 className="text-white text-lg font-semibold mb-1 line-clamp-3">
+                {clip.description_ph}
+              </h3>
+              
+              {/* Don't show description_en if it's empty or if it's the same as description_ph */}
+              {clip.description_en.trim() !== "" &&
+                clip.description_en !== clip.description_ph && (
+                  <p className="text-white text-sm italic mb-1 line-clamp-3">
+                    {clip.description_en}
+                  </p>
+                )}
+            </div>
+          </div>
+        ))}
+      </div>
+
+      {/* Bottom right buttons */}
+      <div className="fixed bottom-8 right-8 flex flex-col items-center space-y-4">
+        <button
+          onClick={showPreviousClip}
+          disabled={clips.size > 0 && currentIndex > 0}
+          className="bg-white p-4 rounded-full shadow-sm hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-dye cursor-pointer"
+        >
+          <span className="text-2xl">↑</span>
+        </button>
+
+        <button
+          onClick={showNextClip}
+          disabled={clips.size > 0 && currentIndex < clips.size - 1}
+          className="bg-white p-4 rounded-full shadow-sm hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-dye cursor-pointer"
+        >
+          <span className="text-2xl">↓</span>
+        </button>
+      </div>
+
+      {/* Top left menu */}
+      <div className="fixed top-8 left-8">
+        <button
+          onClick={handleMenuClick}
+          className="bg-white h-12 w-12 rounded-full shadow-sm hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-dye cursor-pointer"
+        >
+          <span className="text-2xl">☰</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default WatchClips;
